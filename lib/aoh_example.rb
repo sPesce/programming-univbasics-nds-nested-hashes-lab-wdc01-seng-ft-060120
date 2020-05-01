28d598beb7ab74a5aa51bd78a6ad09b6669be60c
stephen pesce <stevepesce879@gmail.com>
2020-05-01 21:23:41 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,6 +16,7 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   literal = []
+  count = 0 
   assembled_aoh.each{|x|
   
     
